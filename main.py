1c7b6279647a6954b4bb0e1292de0c1991ec6a98
Ujjwal Jha <ujjwaljha1225@gmail.com>
2024-02-25 20:20:34 +0545

    File system for tracking income and expense

--- main.py
+++ main.py
@@ -2,15 +2,48 @@ from taipy import Gui
 
 title = "walletWISE 🤑"
 
-hover_text = """
-🚀 Track, Thrive, Triumph.
-#MoneyMastery"""
+hover_text = """🚀 Track, Thrive, Triumph. #MoneyMastery"""
+
+income = ""
+expenses = ""
+
+path = "wallet_calc.gif"
+
+
+def generate():
+    print("Good Work!")
+
+
+def generate_income(state):
+    print(state.income)
+    with open("income.txt", "a") as f:
+        f.write(f"{state.income }")
+
+
+def generate_expenses(state):
+    print(state.expenses)
+    with open("expenses.txt", "a") as f:
+        f.write(f"{state.expenses }")
+
 
 page = """
 <|text-center |
 <|{title}|hover_text="{hover_text}"|text|>
 |>
+
+<|{path}|image|>
+INCOME: <|{income}|input|>
+<|Add to Income!|button|on_action=generate_income|>
+
+EXPENDITURE: <|{expenses}|input|>
+<|Add to Expenses!|button|on_action=generate_expenses|>
+
+<|text-center|
+<|Get Insights|button|on_action=generate|>
+|>
+
 """
+
 
 if __name__ == "__main__":
     app = Gui(page)
