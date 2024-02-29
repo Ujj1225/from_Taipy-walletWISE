6f3c1a78f85fa2903a2c6de727e188b63553e363
Ujjwal Jha <ujjwaljha1225@gmail.com>
2024-02-29 18:23:00 +0545

    Removed all values for user customization

--- expenses_pi_data.py
+++
@@ -1,1 +0,0 @@
-expenses_pi_data = {'Expenses_Source': ['fooding', 'Health', 'Clothing', 'Education', 'Misc', 'Fooding'], 'Amount': [1000.0, 10000.0, 10007000.0, 25000.0, 15000.0, 100000.0]}
--- income_pi_data.py
+++
@@ -1,1 +0,0 @@
-income_pi_data = {'Income_Source': ['business_revenue', 'dividend', 'others', 'salary', 'bank', 'tax_rebate'], 'Amount': [6650.0, 1002449.0, 500.0, 63000.0, 100.0, 20666.0]}
