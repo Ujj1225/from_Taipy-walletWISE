4994c67bfc6d9d1079d13898af7eed2ff5c1b92c
Ujjwal Jha <ujjwaljha1225@gmail.com>
2024-03-04 19:03:15 +0545

    Initialization of Income and Expenses

--- income_pi_data.py
+++ income_pi_data.py
@@ -1,1 +1,1 @@
-income_pi_data = {'Income_Source': ['salary', 'dividend', 'business_revenue', 'tax_rebate'], 'Amount': [10000.0, 5000.0, 1100.0, 200.0]}
+income_pi_data = {'Income_Source': [], 'Amount': []}
